b1065b2eb0d0a28bf47076a292c32f4f20dbba51
Dasha <dasha@example.com>
2021-10-21 17:28:43 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const optimization = () => {
+
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        },
+        runtimeChunk: 'single'
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    }
+
+    return config
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src') 
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'static/images'
+                }
+            },
+            {
+                test: /\.(woff2|woff)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'static/fonts'
+                }
+            }
+        ]
+    }
+}
