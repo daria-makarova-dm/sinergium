25cb8b401e8dd8c2023b84e9eef1089060b0d62f
Dasha <dasha@example.com>
2021-10-25 13:27:56 +0300

    added calculating of size

--- src/index.js
+++ src/index.js
@@ -116,3 +116,134 @@ $(function() {
 
     $(document).on('click', '.js-popup-btn', openPopup);
 });
+
+// change range color
+$(function() {
+
+    let changeColor = (e) => {
+        let value = ($(e.currentTarget).val() - $(e.currentTarget).attr('min'))/($(e.currentTarget).attr('max')-$(e.currentTarget).attr('min'))*100
+        $(e.currentTarget).css('background', `linear-gradient(to right, #ec9e21 0%, #ec9e21 ${value}%, #e7e7e7 ${value}%, #e7e7e7 100%)`)
+    }
+
+    $(document).on('mousemove', '.js-range', changeColor);
+})
+
+// set range inputs values
+$(function() {
+
+    let setOutputValue = (e) => {
+        let rangeId = $(e.currentTarget).attr('id');
+        let output = $(`.js-range-output[data-value=${rangeId}]`);
+        let value = $(e.currentTarget).val();
+
+        output.val(value);
+    }
+
+    let setRangeValue = (e) => {
+        let outputId = $(e.currentTarget).attr('data-value');
+        let range = $(`.js-range[id=${outputId}]`);
+        let value = $(e.currentTarget).val();
+
+        range.val(value);
+
+        let valueForColor = (range.val() - range.attr('min'))/(range.attr('max')-range.attr('min'))*100
+        range.css('background', `linear-gradient(to right, #ec9e21 0%, #ec9e21 ${valueForColor}%, #e7e7e7 ${valueForColor}%, #e7e7e7 100%)`)
+    }
+
+    $(document).on('mousemove', '.js-range', setOutputValue);
+    $(document).on('change', '.js-range-output', setRangeValue);
+    $(document).on('mouseleave', '.js-range-output', setRangeValue);
+})
+
+//choose optimal size
+$(function() {
+
+    let setSize = (e) => {
+
+        let height = +$('#height').val();
+        let weight = +$('#weight').val();
+        let resultOutput = $('.js-size-result');
+        let size;
+
+        if (height >= 145 && height <= 158) {
+            if (weight >= 40 && weight <= 95) {
+                size = 's'
+            } else if (weight >= 96 && weight <= 120) {
+                size = 'm'
+            }
+        } else if (height >= 159 && height <= 168) {
+            if (weight >= 40 && weight <= 62) {
+                size = 's'
+            } else if (weight >= 63 && weight <= 100) {
+                size = 'm'
+            } else if (weight >= 101 && weight <= 120) {
+                size = 'l'
+            }
+        } else if (height >= 169 && height <= 178) {
+            if (weight >= 40 && weight <= 50) {
+                size = 's'
+            } else if (weight >= 51 && weight <= 73) {
+                size = 'm'
+            } else if (weight >= 74 && weight <= 104) {
+                size = 'l'
+            } else if (weight >= 105 && weight <= 120) {
+                size = 'xl'
+            }
+        } else if (height >= 179 && height <= 188) {
+            if (weight >= 40 && weight <= 62) {
+                size = 'm'
+            } else if (weight >= 63 && weight <= 88) {
+                size = 'l'
+            } else if (weight >= 89 && weight <= 104) {
+                size = 'xl'
+            } else if (weight >= 105 && weight <= 120) {
+                size = 'xxl'
+            }
+        } else if (height >= 189 && height <= 195) {
+            if (weight >= 40 && weight <= 50) {
+                size = 'm'
+            } else if (weight >= 51 && weight <= 73) {
+                size = 'l'
+            } else if (weight >= 74 && weight <= 88) {
+                size = 'xl'
+            } else if (weight >= 89 && weight <= 120) {
+                size = 'xxl'
+            }
+        }
+
+        resultOutput.html(size);
+    }
+
+    $(document).on('change', '.js-range', setSize);
+    $(document).on('change', '.js-range-output', setSize);
+})
+
+// set optimal size and close popup
+$(function() {
+
+    let closePopup = (e) => {
+
+        $(e.currentTarget).closest('.js-popup').removeClass('opened');
+        $('.wrapper').removeClass('popup-is-open');
+    }
+
+    let setSizeValue = (e) => {
+
+        let value = $('.js-size-result').html();
+        let sizeRadio = $(`input[name=size][value=${value}]`);
+        let sizeBtn = sizeRadio.closest('.js-size-button');
+
+        if (!sizeBtn.hasClass('disabled')) {
+            sizeRadio.attr('checked', 'true');
+            sizeBtn.addClass('active').siblings().removeClass('active')
+                .find('input[name=size]').removeAttr('checked');
+        } else {
+            sizeBtn.siblings().removeClass('active')
+        }
+
+        closePopup(e);
+    }
+
+    $(document).on('click', '.js-popup-submit', setSizeValue);
+    $(document).on('click', '.js-close-popup', closePopup);
+});
